88de3069b4099914363db4f8f64b113396a57801
Kishan B <bkishan@thoughtworks.com>
2018-11-18 06:54:04 +0530

    Add scaffold from spring initializr

---
+++ src/main/java/com/kishan/springboot2multipledatasources/SpringBoot2MultipleDatasourcesApplication.java
@@ -0,0 +1,12 @@
+package com.kishan.springboot2multipledatasources;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot2MultipleDatasourcesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot2MultipleDatasourcesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/kishan/springboot2multipledatasources/SpringBoot2MultipleDatasourcesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kishan.springboot2multipledatasources;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringBoot2MultipleDatasourcesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
